0e0993c4477f426eafaf6c605dbb66ab2b8180a4
KrishnaTKS <saiitemididapati@gmail.com>
2025-09-14 18:21:08 +0100

    Added compiled-contentlayer-config-OP5MLIMC.mjs.map

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-OP5MLIMC.mjs
@@ -0,0 +1,52 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var CaseStudy = defineDocumentType(() => ({
+  name: "CaseStudy",
+  filePathPattern: `case-studies/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    summary: { type: "string", required: true },
+    sector: { type: "enum", options: ["pharma", "beauty", "fmcg", "other"], required: true },
+    cover: { type: "string", required: true },
+    date: { type: "date", required: true },
+    kpis: {
+      type: "json",
+      required: false
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.replace("case-studies/", "")
+    }
+  }
+}));
+var News = defineDocumentType(() => ({
+  name: "News",
+  filePathPattern: `news/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    summary: { type: "string", required: true },
+    category: { type: "enum", options: ["pharma", "decorative", "tech", "events"], required: true },
+    cover: { type: "string", required: true },
+    date: { type: "date", required: true }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.replace("news/", "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [CaseStudy, News]
+});
+export {
+  CaseStudy,
+  News,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-OP5MLIMC.mjs.map
